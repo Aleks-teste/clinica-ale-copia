17889895447aebc7f25804191b6751007b2ed913
matheusl18 <matheusl280402@gmail.com>
2024-11-28 01:45:43 -0300

    exclusão de arquivo duplicados

--- backend/dbConfig.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/clinicadb', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Conectado ao MongoDB'))
-    .catch(err => console.error('Erro de conexão ao MongoDB:', err));
